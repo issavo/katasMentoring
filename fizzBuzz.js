47e66b808cc042789a5647f32a33a61a4be49e4c
Sonia Orive <isavo70@hotmail.com>
2022-12-21 11:04:05 +0100

    FizzBuzz writes Fizz when the modulus of three is zero

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,17 +1,22 @@
 class FizzBuzz {
 
-    writeFizz(){
-        return "Fizz";
+    writeFizz(aNumber){
+        if(aNumber%3 === 0){
+
+            return "Fizz";
+        }
     }
 }
 describe("FizzBuzz", () => {
     it("Writes Fizz when the number is three", () => {
         //Arrange
-        const aNumber = 3;
         const fizzBuzz = new FizzBuzz();
+        const aNumber = 3;
         //Act
         const numberDivisibleByThree = fizzBuzz.writeFizz(aNumber);
         //Assert
         expect("Fizz").toBe(numberDivisibleByThree);
     });
+
+    
 });
